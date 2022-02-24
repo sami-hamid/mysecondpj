045260f263873f4991552ded7857d36ff60796f8
unknown <sami19942010>
2022-02-24 05:56:08 -0800

    Coverage

--- calculator_test.py
+++ calculator_test.py
@@ -1,33 +1,13 @@
 import unittest
-import calc
 
+# This is the class we want to test. So, we need to import it
+import Calculator as CalculatorClass
 
-class TestCalc(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEqual(calc.add(10, 5), 15)
-        self.assertEqual(calc.add(-1, 1), 0)
-        self.assertEqual(calc.add(-1, -1), -2)
-
-    def test_subtract(self):
-        self.assertEqual(calc.subtract(10, 5), 5)
-        self.assertEqual(calc.subtract(-1, 1), -2)
-        self.assertEqual(calc.subtract(-1, -1), 0)
-
-    def test_multiply(self):
-        self.assertEqual(calc.multiply(10, 5), 50)
-        self.assertEqual(calc.multiply(-1, 1), -1)
-        self.assertEqual(calc.multiply(-1, -1), 1)
-
-    def test_divide(self):
-        self.assertEqual(calc.divide(10, 5), 2)
-        self.assertEqual(calc.divide(-1, 1), -1)
-        self.assertEqual(calc.divide(-1, -1), 1)
-        self.assertEqual(calc.divide(5, 2), 2.5)
-
-        with self.assertRaises(ValueError):
-            calc.divide(10, 0)
-
-
+class Test(unittest.TestCase):
+    calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
+    def test_0_add(self):
+        result = self.calculator.add(4,8)
+        self.assertEqual(result,12)
 if __name__ == '__main__':
+    # begin the unittest.main()
     unittest.main()
